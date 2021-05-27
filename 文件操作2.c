010a5c11c61c22a630c4958c0e4c11f497d65047
ShiYunyang <81097580+Miocaro-hub@users.noreply.github.com>
2021-05-27 21:55:38 +0800

    Update 文件操作2.c

--- 文件操作2.c
+++ 文件操作2.c
@@ -208,3 +208,36 @@ int mian()
 	//从键盘读取,再写入到屏幕
 	return 0;
 }
+
+
+//对比一组函数:
+// scanf /fscanf /sscanf
+// printf/fpritnf/sprintf
+
+//scanf/printf 是针对标准输入流/标准输出流的格式化输出/输入语句
+//fscanf/fprintf 是针对所有输入流/所有输出流的格式化输出/输入语句
+
+
+//sscanf/sprintf sscanf是从字符串中读取格式化的数据 , sprintf是把格式化的数输出(存储到)字符串
+struct S
+{
+	int n;
+	float score;
+	char arr[10];
+};
+int main()
+{
+	struct S s = { 100,3.14,"abcdef" };
+	struct S tmp = { 0 };
+	char buf[1024] = { 0 };
+
+	//把格式化的数据转换成字符串存储到buf
+	sprintf(buf, "%d %f %s", s.n, s.score, s.arr);
+
+	//从buf中读取格式化的数据到tmp中
+	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);
+	printf("%d %f %s\n", tmp.n, tmp.score, tmp.arr);
+	return 0;
+}
+
+//p212 4:00
